16f9a62e4aa62c6d08a4246d71a299952ab68822
Swann Lenik <swann.lenik@boldcommerce.com>
2022-01-25 20:31:52 -0600

    Add Extract DB

---
+++ database/extracts/ladder_migrations.sql
@@ -0,0 +1,13 @@
+INSERT INTO migrations (id, migration, batch) VALUES (1, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (5, '2022_01_18_000000_create_access_rights_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (6, '2022_01_18_000000_create_doubles_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (7, '2022_01_18_000000_create_games_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (8, '2022_01_18_000000_create_groups_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (9, '2022_01_18_000000_create_ladders_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (10, '2022_01_18_000000_create_players_table', 1);
+INSERT INTO migrations (id, migration, batch) VALUES (11, '2022_01_22_000000_create_sets_table', 2);
+INSERT INTO migrations (id, migration, batch) VALUES (13, '2022_01_22_000000_alter_table_games_create_sets_field', 3);
+INSERT INTO migrations (id, migration, batch) VALUES (16, '2022_01_23_000000_add_column_order_groups_table', 4);
